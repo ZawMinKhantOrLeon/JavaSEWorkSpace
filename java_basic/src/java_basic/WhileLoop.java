bb23ec7ab8b264c2b6adeeef81e887cb115ccb27
ZawMinKhantOrLeon <zawminkhantorleon@gmail.com>
2023-11-22 20:14:27 +0630

    WhileLoopAndDoWhileLoop

---
+++ java_basic/src/java_basic/WhileLoop.java
@@ -0,0 +1,19 @@
+package java_basic;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =101;
+		
+		
+		do {
+			System.out.println(x);
+			x++;
+		}
+		while(x <= 100);
+		
+		System.out.println("Out of while loop");
+	}
+
+}
